157576614acb425e93d0c71aa83d922ecc398cd4
Steve Green <stevengreen9000@gmail.com>
2019-09-17 23:06:20 -0400

    use 0 based SIDE array and handle parent/child side calculation in Block

--- wood.py
+++ wood.py
@@ -5,18 +5,21 @@ SIDES_VECTORS = ((0, 1, 0), (0, 0, 1), (1, 0, 0), (0, -1, 0), (0, 0, -1), (-1, 0
 
 
 class Block(object):
-    def __init__(self, coord, parent=None, parent_side=None):
+    def __init__(self, coord, parent=None, parent_side: int = None):
         """
         Up = 0 Front = 1 Right = 2; opposite side is +3
+        :param coord: tuple(x, y, z) of 3d position
+        :param parent: Block object
+        :param parent_side: the child_side from within the parent to get to this object.
+                            To access parent_side from within this object use the inverse.
         """
         self.coord = coord
         self.sides = [None] * len(SIDES)
-        self.parent_side = None
         self.child_side = None
         if 0 <= (parent_side if parent_side is not None else -1) < len(self.sides) and isinstance(parent, Block):
-            self.parent_side = int(parent_side)
-            self.sides[parent_side] = parent
-            parent.set_child(self, (parent_side + 3) % 6)
+            self.parent_side = (parent_side + 3) % 6
+            self.sides[self.parent_side] = parent
+            parent.set_child(self, parent_side)
 
     def set_child(self, child, child_side):
         if 0 <= (child_side if child_side is not None else -1) < len(self.sides) and isinstance(child, Block):
@@ -36,14 +39,14 @@ class Chain(object):
                 if idx == 0 and line != 'S':
                     idx -= 1
                 elif idx == 0 and line == 'S':
-                    self.blocks.append(Block((0, 0, 0)))
+                    self.blocks.append(Block(coord=(0, 0, 0)))
                 elif line not in SIDES:
                     raise ValueError(f'Direction "{line}" is invalid. Expecting: {", ".join(list(SIDES))}')
                 else:
                     self.blocks.append(Block(
-                        tuple(sum(x) for x in zip(self.blocks[idx-1].coord, SIDES_VECTORS[SIDES[line]])),
-                        self.blocks[idx-1],
-                        (SIDES[line] + 3) % 6
+                        coord=tuple(sum(x) for x in zip(self.blocks[idx-1].coord, SIDES_VECTORS[SIDES[line]])),
+                        parent=self.blocks[idx-1],
+                        parent_side=SIDES[line]
                     ))
                 idx += 1
         if idx == 0:
@@ -52,7 +55,7 @@ class Chain(object):
     def print_shape(self):
         side = None
         for b in self.blocks:
-            print(f'{list(SIDES)[side] if side else "S"} {str(b.coord)}')
+            print(f'{list(SIDES)[side] if side is not None else "S"} {str(b.coord)}')
             side = b.child_side
 
 
